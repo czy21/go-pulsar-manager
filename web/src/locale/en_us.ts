5a8885a16bdaafc091cfbc2ea212ecdd9a9fc66a
Bruce Chen <805899926@qq.com>
2022-06-05 14:09:47 +0800

    #

---
+++ web/src/locale/en_us.ts
@@ -0,0 +1,8 @@
+export default {
+    "global.language.description": "English",
+    "table.operation": "Operation",
+    "common.ok": "OK",
+    "common.cancel": "Cancel",
+    "common.filter.reset": "Reset",
+    "common.filter.search": "Search"
+}
---
+++ web/src/locale/zh_cn.ts
@@ -0,0 +1,8 @@
+export default {
+    "global.language.description": "简体中文",
+    "table.operation": "操作",
+    "common.ok": "确认",
+    "common.cancel": "取消",
+    "common.filter.reset": "重置",
+    "common.filter.search": "查询"
+}
