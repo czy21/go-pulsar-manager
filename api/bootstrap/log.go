efc60b8346eea35114a61fd8e537781d3c81734e
Bruce CHEN <805899926@qq.com>
2022-06-13 12:21:15 +0800

    #

--- api/bootstrap/log.go
+++ api/bootstrap/log.go
@@ -1,17 +1,25 @@
 package bootstrap
 
 import (
+	"github.com/czyhome/go-pulsar-manager/exception"
 	"github.com/spf13/viper"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func bootLog() {
 	var logWriter io.Writer = os.Stdout
 	logFile := viper.GetString("log.file")
 	if logFile != "" {
-		f, _ := os.Create(logFile)
+		logFile, err := filepath.Abs(logFile)
+		exception.Check(err)
+		if _, err := os.Stat(logFile); os.IsNotExist(err) {
+			_ = os.MkdirAll(filepath.Dir(logFile), 0777)
+		}
+		f, err := os.Create(logFile)
+		exception.Check(err)
 		logWriter = io.MultiWriter(f)
 	}
 	logger := log.Default()
